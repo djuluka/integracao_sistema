0cc66ba7b63700ee866e74537719c900491f1a56
djuluka <djuluka@gmail.com>
2021-10-21 18:33:51 +0000

    initial project setup

---
+++ src/utad/meitw/application/Application.java
@@ -0,0 +1,14 @@
+package utad.meitw.application;
+/**
+* @author Jailson da Luz Costa djuluka@gmail.com:
+ * @version Creation time: 21 Oct 2021 18:15:45
+ * Class Description
+*/
+public class Application {
+
+	public static void main(String[] args) {
+		System.out.println("Benvindos a Disciplina de Integracao de Sistema");
+
+	}
+
+}
